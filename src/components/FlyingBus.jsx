a942774208d4d27ad3be787d574995537dadedf8
Mr. McCain (aka McOpi) <info@mistermccain.com>
2025-11-04 17:13:52 +0100

    Delete src directory

--- src/components/FlyingBus.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FlyingBus = () => {
-  return null; 
-};
-
-export default FlyingBus;
--- src/config/appConfig.js
+++
@@ -1,7 +0,0 @@
-export const CLOUDIUM_ORDER_WEBHOOK_URL = "https://discord.com/api/webhooks/1374082481594175508/VXpyHkS2usCeh5t61gcil50t-gdXgmnCika3GuU5KrMyAgvajdVzJIqUbHhUT1m87zlc";
-
-export const APPLICATION_FORM_WEBHOOK_URL = "https://discord.com/api/webhooks/1348070308342403082/eYVsLal09i6secL1qwHRKlRIax0ScHhCyMStoJL9sd2tWTeT3KjmAb4EySm6uNP-9GtI";
-
-export const CLOUDIUM_SUPPORT_WEBHOOK_URL = "https://discord.com/api/webhooks/1219382645933805628/j9B6XQVJSE746RWBX3Z1QhI2VtKxS8N4yXkFjWpWxJ3uY7rGcE0M8pD1OaFzN9lRjJ2q";
-
-export const DISCORD_SERVER_INVITE_URL = "https://discord.gg/busbahnhof";
